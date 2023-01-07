6299807d58c3958d02d40e6a6bce86e243e0eec3
valentin1609 <valentin160901@gmail.com>
2023-01-07 06:20:57 -0300

    encriptado por contraseña

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+var getPassword = document.getElementById("password");
+var getInput = document.getElementById("input");
 
+
+
+
+function onEncrypt() {
+  let password = getPassword.value;
+  let input = getInput.value;
+
+
+  if (password == "") {
+
+    //codificacion por defecto
+
+  } 
+  else {  //codificacion por contraseña
+    let wordCode = getWordCode(password);
+    let newText = encrypt(input, wordCode);
+
+    document.getElementById("output").textContent = newText;
+  }
+
+}
+
+
+
+function getWordCode(password) {
+    let wordCode = 0;
+    for (let i = 0; i < password.length; i++) {
+        wordCode += password[i].codePointAt();
+//************si la palabra tiene las mismas letras, tendra el mismo codigo******************************//
+    }
+    return wordCode;
+}
+
+function encrypt (input, wordCode) {
+
+    let newText = ""; 
+    for (let i = 0; i < input.length; i++) {    
+        let letterCode = input[i].codePointAt();  //obtengo el codigo de la letra 
+        let newLetterCode = letterCode + wordCode;   //le sumo el valor de la contraseña para obtener otra letra
+        let newLetter = String.fromCodePoint(newLetterCode);    //desencripto la nueva letra
+        newText = newText + newLetter;    //agrego la letra al nuevo texto
+    }
+    return newText;
+//*********si el codigo de la letra + la contraseña es muy alto sobrepasara el valor de unicode: Invalid code point*************/
+}
