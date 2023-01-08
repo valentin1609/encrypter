35afe844df8fd60f8196273ff784627349e86dce
valentin1609 <valentin160901@gmail.com>
2023-01-08 06:14:13 -0300

    animacion añadida y solucionado el error de unicode negativo

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ function onEncrypt() {
     //codificacion por defecto()
     let newText = defaultEncrypt(input);
 
-    document.getElementById("output").textContent = newText;
-  } 
-  else {
+    // document.getElementById("output").textContent = newText;
+    animation(newText);
+  } else {
     //codificacion por contraseña
     let wordCode = getWordCode(password);
     let newText = encrypt(input, wordCode);
 
-    document.getElementById("output").textContent = newText;
+    // document.getElementById("output").textContent = newText;
+    animation(newText);
   }
 }
 
@@ -35,15 +36,16 @@ function onDecrypt() {
 
   if (password == "") {
     //decodificacion por defecto()
-    let oldText = defaultDecrypt(input);
+    let originText = defaultDecrypt(input);
+    // document.getElementById("output").textContent = originText;
+    animation(originText);
 
-    document.getElementById("output").textContent = oldText;
-  } 
-  else {
+  } else {
     let wordCode = getWordCode(password);
-    let newText = decrypt(input, wordCode);
+    let originText = decrypt(input, wordCode);
 
-    document.getElementById("output").textContent = newText;
+    // document.getElementById("output").textContent = originText;
+    animation(originText);
   }
 }
 
@@ -51,19 +53,20 @@ function encrypt(input, wordCode) {
   let newText = "";
   for (let i = 0; i < input.length; i++) {
     //obtengo el codigo de la letra:
-    let letterCode = input[i].codePointAt(); 
+    let letterCode = input[i].codePointAt();
     //le sumo el valor de la contraseña para obtener otra letra:
-    let newLetterCode = letterCode + wordCode; 
+    let newLetterCode = letterCode + wordCode;
     //obtengo la nueva letra a partir del nuevo codigo:
-    let newLetter = String.fromCodePoint(newLetterCode); 
-    newText = newText + newLetter; 
+    let newLetter = String.fromCodePoint(newLetterCode);
+    newText = newText + newLetter;
   }
   return newText;
-//si el codigo de la letra + la contraseña es muy alto sobrepasara el valor de unicode.
+  //si el codigo de la letra + la contraseña es muy alto sobrepasara el valor de unicode.
 }
 
 function decrypt(input, wordCode) {
-  let newText = "";
+  try {
+    let newText = "";
   for (let i = 0; i < input.length; i++) {
     let letterCode = input[i].codePointAt();
     let newLetterCode = letterCode - wordCode; //en lugar de sumar: restar
@@ -71,7 +74,14 @@ function decrypt(input, wordCode) {
     newText = newText + newLetter;
   }
   return newText;
+  } catch (error) {
+    //si la contraseña no es correcta puede resultar un codigo negativo
+    alert("contraseña invalida");
+  }
+  
 }
+
+
 
 function defaultEncrypt(input) {
   let newText = "";
@@ -114,4 +124,14 @@ function defaultDecrypt(input) {
 function onCopy() {
   var text = document.getElementById("output").innerText;
   navigator.clipboard.writeText(text);
+}
+
+function animation(newText) {
+  document.getElementById("output").textContent = "";
+
+  for (let i = 0; i < newText.length; i++) {
+          setTimeout(function() {
+            document.getElementById("output").textContent = newText.substring(0, i+1);
+          }, 100 * i);
+  }
 }
